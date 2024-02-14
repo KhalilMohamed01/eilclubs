bc83e7c3204a840bf87a749fc4340d8435b113e9
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-02-14 13:43:11 +0100

    allow all cors

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,7 @@ const app = express()
 
 const cors = require('cors');
 //allow all
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
+app.use(cors());
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
