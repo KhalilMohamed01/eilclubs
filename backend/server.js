d2afbbb5c4b2c13bada7b0060274cc7d8f481e68
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-02-14 13:51:05 +0100

    adding headers to server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const app = express()
 
 const cors = require('cors');
 //allow all
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+ 
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
