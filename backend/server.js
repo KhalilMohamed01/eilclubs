6500e736c899c7270ffccc685f5441f824a72c72
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-02-16 20:09:19 +0100

    setting headers manually

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
  
 app.use(express.json())
 app.use((req, res, next) => {
