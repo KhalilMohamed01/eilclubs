074aefe69c08667aa9bbb06bc49c46dd39349dd1
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-02-16 20:21:20 +0100

    trying verceljson for cors

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,6 @@ var corsOptions = {
   }
 app.use(cors(corsOptions))
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
  
 app.use(express.json())
 app.use((req, res, next) => {
