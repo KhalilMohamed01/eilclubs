651584c39c5e09b8ada696a7982e192a5310b3cd
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-02-17 12:55:44 +0100

    test 702

--- backend/server.js
+++ backend/server.js
@@ -6,25 +6,19 @@ const eventRoutes = require('./routes/events')
 const clubRoutes = require('./routes/clubs')
 const app = express()
 
-//const cors = require('cors');
+const cors = require('cors');
 //allow all
 // var corsOptions = {
 //     origin: 'https://eilclubs-git-test-khalilmohamed01s-projects.vercel.app',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 //   }
 
-//app.use(cors())
+app.use(cors())
 
  
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
+
     console.log(req.path, req.method)
     next()
 })
